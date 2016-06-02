c08dbe5ac8f4a7af3a355d790a33034ac9b8c703
Christian Hernandez <christian.hernandez@redhat.com>
2016-06-02 13:10:44 -0700

    updated cgi

---
+++ cgi-bin/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Content-type: text/html"
+echo "<html>"
+echo "<head>"
+echo "<title>CGI Test</title>"
+echo "</head>"
+echo "<body>"
+echo "<a href="index.html">Click Here</a>"
+echo "</body>"
+echo "</html>"
+##
+##
